631937f7dd6508a78bc8c97ce403bfb51800cdda
Melody <melody.rockwell3@gmail.com>
2021-01-27 13:49:47 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Game from "./component/game/Game";
+import Scoreboard from "./component/scoreboard/Scoreboard";
+import Team from "./component/team/Team";
 
 function App() {
+  const redPandas = {
+    name: "Roseville Red Pandas",
+    logo:
+      "https://image.freepik.com/free-vector/red-panda-mascot-esport-logo-design_139366-360.jpg",
+  };
+
+  const polarBears = {
+    name: "Polaris Polar Bears",
+    logo:
+      "https://image.freepik.com/free-vector/polar-bear-mascot-esport-illustration_188253-56.jpg",
+  };
+
+  const elephants = {
+    name: "Edmondton Elephants",
+    logo:
+      "https://image.freepik.com/free-vector/elephant-sport-mascot-logo-design_167995-478.jpg",
+  };
+
+  const dolphins = {
+    name: "Delta Dolphins",
+    logo:
+      "https://image.freepik.com/free-vector/dolphin-esport-logo-mascot-design_157186-410.jpg",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Colony Arena"
+        homeTeam={redPandas}
+        visitingTeam={polarBears}
+      />
+      <Game venue="Polaris Park" homeTeam={elephants} visitingTeam={dolphins} />
     </div>
   );
 }
